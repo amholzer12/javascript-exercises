e4ff721e6f63f53c46059053bb80983d646faa16
Austin Holzer <amholzer12@gmail.com>
2022-10-07 14:59:25 -0700

    complete into exercise and exercise two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let answer =""
+    if (n < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < n; i++) {
+        answer += word
+    }
+    return answer
 };
 
 // Do not edit below this line
