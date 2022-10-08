c9b0995282018ab762ce13fb85c36e2db22e477e
Austin Holzer <amholzer12@gmail.com>
2022-10-08 14:04:19 -0700

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(pants) {
+    wordArray = Array.from(pants)
+    let answer = ""
+    for (i = 0; i < pants.length; i++) {
+        const letter = wordArray.pop()
+        answer += letter
+}
+    return answer
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
