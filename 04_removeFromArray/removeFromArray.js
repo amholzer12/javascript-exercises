96128db733c6c8d4f2e17cd259baef73842b5feb
Austin Holzer <amholzer12@gmail.com>
2022-10-09 13:29:35 -0700

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray,...takeOut) {
 
+    for (i = 0; i < takeOut.length; i++) {
+        let eleIndex = oldArray.indexOf(takeOut[i])
+        if (eleIndex > -1) {
+            oldArray.splice(eleIndex, 1)
+        }
+    }
+    return oldArray
+//I need to take an array, then target and remove a specific element and then return the new array
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
