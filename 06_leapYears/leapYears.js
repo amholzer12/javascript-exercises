81301adb42bcd56459e98ecbb7cbfc9f6a48ad0c
Austin Holzer <amholzer12@gmail.com>
2022-10-12 13:52:38 -0700

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 400 == 0 && year % 100 == 0) {
+            return true
+        } else if (year % 100 == 0) {
+            return false
+        }
+        return true
+    } else {
+        return false
+    }
 
+
+//create a series of if checks to determine whether or not it is a leap year.
+//year CAN be devided 4
+//year can NOT be divided by 100!!!<---bait, check if it is divisible by 100 and then return false
+//year CAN be devided by 400
 };
 
 // Do not edit below this line
