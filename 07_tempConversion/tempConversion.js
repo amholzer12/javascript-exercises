4983a01cf127b8c7f8dc9cfcdb9304ba0df18006
Austin Holzer <amholzer12@gmail.com>
2022-10-12 14:33:52 -0700

    complete final exercise assigned, exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  answer = (f -32) * (5/9);
+  roundedAnswer = Math.round(answer * 10) / 10
+  return roundedAnswer
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  answer = c * 1.8 + 32;
+  roundedAnswer = Math.round(answer * 10) / 10
+  return roundedAnswer
 };
 
 // Do not edit below this line
